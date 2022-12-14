dfab557c0b7db512f7c21f8b9cc787cf86f04efe
JoongHyuk Shin <93418349+Joshua-Shin@users.noreply.github.com>
2022-12-14 17:54:16 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,60 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-struct Node
-{
-    int left;
-    int right;
-};
-Node a['Z'];
-
-void preorder(int n)
-{
-    if(n==-1)
-        return;
-    cout << (char)(n + 'A');
-    preorder(a[n].left);
-    preorder(a[n].right);
+int lc[27]; // lc[x] = x 노드의 왼쪽 자식노드 값.
+int rc[27]; // rc[x] = x 노드의 오른쪽 자식 노드 값.
+void preOrder(int x) {
+    cout << char(x - 1 + 'A');
+    if(lc[x]) preOrder(lc[x]);
+    if(rc[x]) preOrder(rc[x]);
 }
-void inorder(int n)
-{
-    if(n==-1)
-        return;
-    inorder(a[n].left);
-    cout << (char)(n + 'A');
-    inorder(a[n].right);
+void inOrder(int x) {
+    if(lc[x]) inOrder(lc[x]);
+    cout << char(x - 1 + 'A');
+    if(rc[x]) inOrder(rc[x]);
 }
-void postorder(int n)
-{
-    if(n==-1)
-        return;
-    postorder(a[n].left);
-    postorder(a[n].right);
-    cout << (char)(n + 'A');
+void postOrder(int x) {
+    if(lc[x]) postOrder(lc[x]);
+    if(rc[x]) postOrder(rc[x]);
+    cout << char(x - 1 + 'A');
 }
-int main()
-{
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
-    for(int i =0; i<n; i++)
-    {
-        char x, y, z;
-        cin >> x >> y >> z;
-        if(y!='.')
-            a[x-'A'].left = y - 'A';
-        else
-            a[x-'A'].left = -1;
-
-        if(z!='.')
-            a[x-'A'].right = z - 'A';
-        else
-            a[x-'A'].right = -1;
+    for (int i = 0; i < n; i++) {
+        char a, b, c;
+        cin >> a >> b >> c;
+        if(b!='.') lc[a - 'A' + 1] = b - 'A' + 1;
+        if(c!='.') rc[a - 'A' + 1] = c - 'A' + 1;
     }
 
-    preorder(0);
+    preOrder(1);
     cout << '\n';
-    inorder(0);
+    inOrder(1);
     cout << '\n';
-    postorder(0);
+    postOrder(1);
     cout << '\n';
+
     return 0;
 }
