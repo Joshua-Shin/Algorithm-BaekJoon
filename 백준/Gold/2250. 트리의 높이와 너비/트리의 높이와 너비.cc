31c961f89778da08686228e4d9730052be202596
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2022-12-31 12:54:54 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 4 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
@@ -1,30 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int lc[10001];
-int rc[10001]; // rc[x] = x의 오른쪽 자식 노드
-int row[10001]; // row[x] = 정점 x의 행 값
-int col[10001]; // col[x] = 정점 x의 열 값
-int maxLc[10001]; // maxLc[x] = 레벨이 x인 노드 중 왼쪽 노드 번호
-int maxRc[10001];
-int root;
 int p[10001];
+int lc[10001], rc[10001]; // rc[x] = x의 오른쪽 자식 노드
+int row[10001], col[10001]; // col[x] = 정점 x의 열 값
+pair<int, int> mostLRCol[10001]; // 높이가 x일때, 가장 왼쪽 열값, 가장 오른쪽 열값
+int root;
 int bfs(int x) {
     int maxLevel = 1;
     queue<int> q;
     row[x] = 1;
-    maxLc[row[x]] = x;
-    maxRc[row[x]] = x;
     q.push(x);
     while(!q.empty()) {
         x = q.front();
         maxLevel = max(maxLevel, row[x]);
-        // cout << "x: " << x << '\n';
-        // cout << "row[x]: " << row[x] << '\n';
-        // cout << "maxLc[row[x]]: " << maxLc[row[x]] << '\n';
-        // cout << "col[maxLc[row[x]]]: " << col[maxLc[row[x]]] << '\n';
-        if(col[x] < col[maxLc[row[x]]] || maxLc[row[x]]==0) maxLc[row[x]] = x;
-        if(col[x] > col[maxRc[row[x]]]) maxRc[row[x]] = x;
         q.pop();
         if(lc[x]!=-1){
             row[lc[x]] = row[x] + 1;
@@ -40,7 +29,10 @@ int bfs(int x) {
 int colValue = 1;
 void inOrder(int x) {
     if(lc[x]!=-1) inOrder(lc[x]);
-    col[x] = colValue++;
+    colValue++;
+    auto &[lCol, rCol] = mostLRCol[row[x]];
+    if(!lCol || colValue < lCol) lCol = colValue;
+    if(!rCol || colValue > rCol) rCol = colValue;
     if(rc[x]!=-1) inOrder(rc[x]);
 }
 void findRoot(int x) {
@@ -58,21 +50,18 @@ int main() {
         cin >> a >> b >> c;
         lc[a] = b;
         rc[a] = c;
-        p[b] = a;
-        p[c] = a;
+        if(b!=-1)p[b] = a;
+        if(c!=-1)p[c] = a;
     }
     findRoot(1);
-    inOrder(root);
     int maxLevel = bfs(root);
-    int answerWidth = 1;
-    int answerLevel = 1;
+    inOrder(root);
+    int answerWidth = 1, answerLevel = 1;
     for(int i = 1; i<=maxLevel; i++) {
-        int left = maxLc[i];
-        int right = maxRc[i];
-        int width = col[right] - col[left] + 1;
+        int width = mostLRCol[i].second - mostLRCol[i].first + 1;
         if(width > answerWidth) {
             answerWidth = width;
-            answerLevel = row[left];
+            answerLevel = i;
         }
     }
     cout << answerLevel << ' ' << answerWidth << '\n';
