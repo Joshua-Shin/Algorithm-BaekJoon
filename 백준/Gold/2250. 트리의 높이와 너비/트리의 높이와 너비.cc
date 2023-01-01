1f8f82728a942d7ac863c2535f4f5ddc39c5de2c
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-01-01 13:43:11 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
@@ -1,50 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
+int lc[10001], rc[10001];
 int p[10001];
-int lc[10001], rc[10001]; // rc[x] = x의 오른쪽 자식 노드
-int row[10001]; // rol[x] = 정점 x의 행 값
-pair<int, int> mostLRCol[10001]; // 높이가 x일때, 가장 왼쪽 열값, 가장 오른쪽 열값
 int root;
-int maxLevel = 1;
-int colValue = 1;
+int maxLevel = 0;
+int colValue = 0;
+pair<int, int> colLR[10001]; // colLR[x] : 높이가 x일떄 가장 왼쪽 노드의 열값과 가장 오른쪽 노드의 열값
 void inOrder(int x, int d) {
     if(d > maxLevel) maxLevel = d;
-    if(lc[x]!=-1) inOrder(lc[x], d + 1);
+    if(lc[x]!=-1) inOrder(lc[x], d+1);
+    auto &[leftCol, rightCol] = colLR[d];
     colValue++;
-    auto &[lCol, rCol] = mostLRCol[d];
-    if(!lCol || colValue < lCol) lCol = colValue;
-    if(!rCol || colValue > rCol) rCol = colValue;
-    if(rc[x]!=-1) inOrder(rc[x], d + 1);
+    if(!leftCol || leftCol > colValue) leftCol = colValue;
+    if(!rightCol || rightCol < colValue) rightCol = colValue;
+    if(rc[x]!=-1) inOrder(rc[x], d+1);
 }
-void findRoot(int x) {
-    if(p[x]==0) {
-        root = x;
-        return;
-    }
-    findRoot(p[x]);
+int findRoot(int x) {
+    if(p[x]==0) return x;
+    return findRoot(p[x]);
 }
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        lc[a] = b;
-        rc[a] = c;
-        if(b!=-1)p[b] = a;
-        if(c!=-1)p[c] = a;
+        lc[a] = b; rc[a] = c;
+        if(b!=-1) p[b] = a;
+        if(c!=-1) p[c] = a;
     }
-    findRoot(1);
+    root = findRoot(1);
     inOrder(root, 1);
-    int answerWidth = 1, answerLevel = 1;
-    for(int i = 1; i<=maxLevel; i++) {
-        int width = mostLRCol[i].second - mostLRCol[i].first + 1;
-        if(width > answerWidth) {
-            answerWidth = width;
-            answerLevel = i;
+    int ansLevel = 1, ansWidth = 1;
+    for(int i = 1; i <= maxLevel; i++) {
+        int width = colLR[i].second - colLR[i].first + 1;
+        if(ansWidth < width) {
+            ansWidth = width;
+            ansLevel = i;
         }
     }
-    cout << answerLevel << ' ' << answerWidth << '\n';
+    cout << ansLevel << ' ' << ansWidth << '\n';
     return 0;
 }
