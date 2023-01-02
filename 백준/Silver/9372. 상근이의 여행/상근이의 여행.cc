35bb641acf7fc6fedaddf157277761ac2a8d4cbd
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-01-02 20:12:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int tc;
+    cin >> tc;
+    while(tc--) {
+        int n, m;
+        cin >> n >> m;
+        while(m--) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n-1 << '\n';
+    }
+    return 0;
+}
