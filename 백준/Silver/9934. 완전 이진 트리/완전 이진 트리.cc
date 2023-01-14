e0af80cb36f5df0e08e2174994d0e458e7a3ecbe
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-01-14 12:24:22 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -2,20 +2,13 @@
 using namespace std;
 int n, level;
 int arr[2000];
-vector<pair<int, int>> ans; // 층, 값 // stable_sort
+vector<int> ans[12]; // ans[x]: x 층일떄의 값
 void go(int st, int en, int d) {
-    if(st==en) return;
-    if(st==en-1) {
-        ans.push_back({d, arr[st]});
-        return;
-    }
+    if(st >= en) return;
     int mid = (st + en) / 2;
-    ans.push_back({d, arr[mid]});
+    ans[d].push_back(arr[mid]);
     go(st, mid, d+1);
     go(mid + 1, en, d+1);
-}
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    return a.first < b.first;
 }
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -23,20 +16,9 @@ int main() {
     n = pow(2, level) - 1;
     for (int i = 0; i < n; i++) cin >> arr[i];
     go(0, n, 0);
-    stable_sort(ans.begin(), ans.end(), cmp);
-    // for(auto x: ans) {
-    //     cout << x.first << ' ' << x.second << '\n';
-    // }
-    // cout << "=======" << '\n';
-    int cntLevel = 0;
-    for(int i = 0; i<ans.size(); i++) {
-        if(ans[i].first==cntLevel) {
-            cout << ans[i].second << ' ';
-        }
-        else {
-            cntLevel++;
-            cout << '\n';
-            i--;
-        }
+    for(int i = 0; i<level; i++) {
+        for(auto x : ans[i])
+            cout << x << ' ';
+        cout << '\n';
     }
 }
