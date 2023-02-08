8ea9f0a3e4a679cfcaecad4dfdba80525f207e63
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-08 19:28:15 +0900

    [unrated] Title: 롤케이크 자르기, Time: 159.61 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solution(vector<int> topping) {
+    int answer = 0;
+    map<int, int> m1, m2;
+    for(int i = 0; i<topping.size(); i++) m2[topping[i]]++;
+    for(int i = 0; i<topping.size(); i++) {
+        m1[topping[i]]++;
+        m2[topping[i]]--;
+        if(!m2[topping[i]]) m2.erase(topping[i]);
+        if(m1.size()==m2.size()) answer++;
+    }
+    return answer;
+}
