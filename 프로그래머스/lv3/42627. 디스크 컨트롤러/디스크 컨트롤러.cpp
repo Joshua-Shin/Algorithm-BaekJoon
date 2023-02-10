bfe08a77eb63793b259aad51a6346f65f7fbbfa9
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-10 14:50:03 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int solution(vector<vector<int>> jobs) {
-    int totalTime = 0, currentTime = 0, cur = 0;
-    sort(jobs.begin(), jobs.end());
-    // 최소힙. 소요시간을 앞에다 저장
+    int totalTime = 0;
+    sort(jobs.begin(), jobs.end()); // 요청시점, 소요시간
+    int currentTime = 0;
+    int i = 0;
+    // 소요시간, 요청시점
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    while(cur < jobs.size() || !pq.empty()) {
-        //요청시간이 현재 소요된 시간보다 작을때 작업을 수행할 수 있음
-        if(cur < jobs.size() && jobs[cur][0] <= currentTime) {
-            pq.push({jobs[cur][1], jobs[cur][0]});
-            cur++;
+    while(i < jobs.size() || !pq.empty()) {
+        if(i < jobs.size() && currentTime >= jobs[i][0]) {
+            pq.push({jobs[i][1], jobs[i][0]});
+            i++;
             continue;
         }
-        // 수행할 작업이 있어.
-        if(!pq.empty()) {
-            currentTime += pq.top().first;
-            totalTime += currentTime - pq.top().second;
+        if(pq.empty()) {
+            currentTime = jobs[i][0];
+        } else {
+            auto [cost, startTime] = pq.top();
             pq.pop();
-        }
-        // 수행할 작업이 없고, 현재시간 안에 들어온 작업이 없을 경우 현재시간 조정
-        else {
-            currentTime = jobs[cur][0];
+            currentTime += cost;
+            totalTime += currentTime - startTime;
         }
     }
     return totalTime / jobs.size();
