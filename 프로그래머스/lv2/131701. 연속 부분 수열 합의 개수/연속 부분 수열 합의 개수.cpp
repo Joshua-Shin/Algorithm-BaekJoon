3ade5dd3a4773eff3697ff86b3fde358a90b69b4
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-10 13:30:04 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 66.28 ms, Memory: 7.22 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int solution(vector<int> elements) {
-    set<int> s;
     int n = elements.size();
+    vector<int> v;
     for (int i = 0; i < n; i++) {
         int sum = 0;
-        for (int j = i; j < i + n; j++) {
+        for (int j = i; j < n + i; j++) { 
             sum += elements[j % n];
-            s.insert(sum);
+            v.push_back(sum);
         }
     }
-    return s.size();
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    return v.size();
 }
