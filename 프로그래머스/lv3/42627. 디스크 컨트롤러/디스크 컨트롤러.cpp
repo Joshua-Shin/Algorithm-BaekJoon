289bcec6cf55fb5f9f68805033bc8c3759edfef3
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-11 15:58:00 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int solution(vector<vector<int>> jobs) {
-    int totalTime = 0;
-    sort(jobs.begin(), jobs.end()); // 요청시점, 소요시간
-    int currentTime = 0;
-    int i = 0;
-    // 소요시간, 요청시점
+    int totalTime = 0, cur = 0, currentTime = 0;
+    sort(jobs.begin(), jobs.end());
+    // 소요시간, 요청시각
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    while(i < jobs.size() || !pq.empty()) {
-        if(i < jobs.size() && currentTime >= jobs[i][0]) {
-            pq.push({jobs[i][1], jobs[i][0]});
-            i++;
+    while(cur < jobs.size() || !pq.empty()) {
+        if(cur < jobs.size() && currentTime >= jobs[cur][0]) {
+            pq.push({jobs[cur][1], jobs[cur++][0]});
             continue;
         }
-        if(pq.empty()) {
-            currentTime = jobs[i][0];
-        } else {
-            auto [cost, startTime] = pq.top();
+        if(!pq.empty()) {
+            auto [cost, requestTime] = pq.top();
             pq.pop();
             currentTime += cost;
-            totalTime += currentTime - startTime;
-        }
+            totalTime += (currentTime - requestTime);
+        } else currentTime = jobs[cur][0];
     }
     return totalTime / jobs.size();
 }
