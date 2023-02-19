fa6e952338b140aa163b3426b87830cb47ff8822
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-19 13:27:26 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.cc
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -2,9 +2,12 @@
 using namespace std;
 int n, m, temp;
 bool vip[41];
+int cache[41][2];
 int dfs(int idx, bool prevSwitch) {
     if(idx==n+1) return 1;
-    int ret = 0;
+    int &ret = cache[idx][prevSwitch];
+    if(ret != -1) return ret;
+    ret = 0;
     if(vip[idx]) return ret += dfs(idx+1, false);
     if(prevSwitch) return ret += dfs(idx+1, false);
     else {
@@ -21,5 +24,6 @@ int main() {
         cin >> temp;
         vip[temp] = true;
     }
+    memset(cache, -1, sizeof(cache));
     cout << dfs(1, 0);
 }
