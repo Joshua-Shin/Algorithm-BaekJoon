796966518b787be6755c6219ad0eccd0e3237fde
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-21 13:40:05 +0900

    Create 외발 뛰기.cpp

---
+++ 알고스팟/외발 뛰기/외발 뛰기.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int tc, n, board[101][101];
+vector<string> ans;
+int cache[101][101];
+bool dp(int x, int y) {
+    if (x == n - 1 && y == n - 1) return true;
+    int &ret = cache[x][y];
+    if (ret != -1) return ret;
+    int ny = y + board[x][y];
+    int nx = x + board[x][y];
+    bool result1 = false, result2 = false;
+    if (0 <= ny && ny < n)
+        result1 = dp(x, y + board[x][y]);
+    if (0 <= nx && nx < n)
+        result2 = dp(x + board[x][y], y);
+    return ret = result1 || result2;
+}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> tc;
+    while (tc--) {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> board[i][j];
+        memset(cache, -1, sizeof(cache));
+        dp(0, 0) ? ans.push_back("YES") : ans.push_back("NO");
+    }
+    for (auto x : ans)
+        cout << x << '\n';
+}
