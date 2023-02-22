8cc7bf98c5dad306c70997ea330660332b70830e
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-22 17:45:09 +0900

    Update WILDCARD.cpp

--- 알고스팟/WILDCARD.cpp
+++ 알고스팟/WILDCARD.cpp
@@ -3,37 +3,34 @@
 using namespace std;
 int tc, n;
 string W, S;
-vector<string> ans;
-vector<vector<string>> totalAns;
+vector<vector<string>> ans;
 int cache[101][101];
 int dfs(int w, int s) {
     if(w == W.size() && s == S.size()) return 1;
     int &ret = cache[w][s];
     if(ret != -1) return ret;
-    if(w < W.size() && s < S.size() && (W[w] == '?' || W[w] == S[s]))
-        return ret = dfs(w+1, s+1);
-    if(W[w] == '*') {
-        if(dfs(w+1, s) || (s < S.size() && dfs(w, s+1)))
-            return ret = 1;
+    if(s < S.size() && w < W.size()) {
+        if(W[w] == '?' || W[w] == S[s]) return ret = dfs(w+1, s+1);
+        if(W[w] == '*') return ret = dfs(w+1, s) || dfs(w, s+1);
     }
+    if(s == S.size() && W[w] == '*')
+        return ret = dfs(w+1, s);
     return ret = 0;
 }
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
     cin >> tc;
     while(tc--) {
-        cin >> W;
-        cin >> n;
-        ans.clear();
+        cin >> W >> n;
+        vector<string> subAns;
         for (int i = 0; i < n; i++) {
             cin >> S;
             memset(cache, -1, sizeof(cache));
-            if(dfs(0, 0)) ans.push_back(S);
+            if(dfs(0, 0)) subAns.push_back(S);
         }
-        sort(ans.begin(), ans.end());
-        totalAns.push_back(ans);
+        sort(subAns.begin(), subAns.end());
+        ans.push_back(subAns);
     }
-    for(auto v: totalAns)
+    for(auto v: ans)
         for(auto x: v)
             cout << x << '\n';
 }
