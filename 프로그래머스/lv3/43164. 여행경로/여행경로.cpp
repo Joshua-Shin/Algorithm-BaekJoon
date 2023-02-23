5ac3aaca3cf5cab0da9b275c0ec83438912e0c7f
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-23 20:21:36 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<bool> check;
 vector<string> answer;
-bool dfs(string start, vector<vector<string>> &tickets, vector<bool> &check, int cnt) {
+vector<vector<string>> tickets;
+bool dfs(string start, int cnt) {
     answer.push_back(start);
     if(cnt == tickets.size()) return true;
     for(int i = 0; i<tickets.size(); i++) {
-        if(tickets[i][0] == start && check[i]==false) {
-            check[i] = true;
-            if(dfs(tickets[i][1], tickets, check, cnt+1)) return true;
-            check[i] = false;
-        }
+        if(tickets[i][0] != start) continue;
+        if(check[i]) continue;
+        check[i] = true;
+        if(dfs(tickets[i][1], cnt + 1)) return true;
+        check[i] = false;
     }
     answer.pop_back();
     return false;
 }
 vector<string> solution(vector<vector<string>> tickets) {
-    vector<bool> check(tickets.size(), false);
+    check.resize(tickets.size());
     sort(tickets.begin(), tickets.end());
-    dfs("ICN", tickets, check, 0);
+    ::tickets = tickets;
+    dfs("ICN", 0);
     return answer;
 }
