f0893653d40fc7c6e58801cc79e365edc4eabe7b
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-25 13:22:35 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 14.00 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,36 +1,24 @@
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int dp[1001][1001];
-
-int dfs(int i, int j, vector<vector<int>>& board) {
-    if(i == 0 || j == 0) return board[i][j]; // board의 가장자리인 경우 현재 칸 값 반환
-    
-    if(dp[i][j] != -1) return dp[i][j]; // memoization
-    
-    int result = 0;
-    if(board[i][j] == 1) { // 현재 칸 값이 1인 경우
-        result = min({dfs(i-1, j-1, board), dfs(i-1, j, board), dfs(i, j-1, board)}) + 1;
-        // 왼쪽 위, 위쪽, 왼쪽 대각선 방향의 칸 중 가장 작은 값에 1을 더한 값이 현재 칸까지 이어지는 가장 큰 정사각형의 한 변의 길이
-    }
-    dp[i][j] = result; // memoization
-    return result;
+int cache[1001][1001];
+int dfs(int x, int y, vector<vector<int>> &board) {
+    if(x == 0 || y == 0) return board[x][y];
+    int &ret = cache[x][y];
+    if(ret != -1) return ret;
+    if(!board[x][y]) return ret = 0;
+    ret = min({dfs(x-1, y, board), dfs(x, y-1, board), dfs(x-1, y-1, board)}) + 1;
+    return ret;
 }
 
 int solution(vector<vector<int>> board) {
     int answer = 0;
-    int n = board.size(), m = board[0].size();
-    
-    fill(&dp[0][0], &dp[1000][1001], -1); // dp 배열 -1로 초기화
-    
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
+    int n = board.size();
+    int m = board[0].size();
+    memset(cache, -1, sizeof(cache));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j< m ; j++) {
             answer = max(answer, dfs(i, j, board));
-            // dfs를 통해 각 칸에서 이어지는 가장 큰 정사각형의 한 변의 길이를 구하고, 그 중 가장 큰 값을 answer에 저장
         }
     }
-    
-    return answer * answer; // 가장 큰 정사각형의 넓이 반환
+    return answer * answer;
 }
