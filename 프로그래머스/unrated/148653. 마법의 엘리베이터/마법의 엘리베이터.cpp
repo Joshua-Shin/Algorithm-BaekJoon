fd6a8c313cce01b640cf970b936b820ea9bf1104
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-27 21:39:44 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int dfs(int n) {
-    if(n == 0) return 0;
+    if(n < 10) return min(n, 10 -n + 1);
     int temp = n % 10;
-    int ret = dfs(n / 10) + temp;
-    if(temp >= 5) ret = min(dfs(n / 10 + 1) + (10 - temp), ret);
-    return ret;
+    return min(dfs(n / 10 + 1) + (10 - temp), dfs(n / 10) + temp);
 }
 int solution(int storey) {
     int answer = 0;
