b7f5a1b8f0b4b2f5abdb4844883a3d110c94eeda
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-27 19:57:03 +0900

    [level 2] Title: 하노이의 탑, Time: 25.19 ms, Memory: 29.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<vector<int>> answer;
-void hanoi(int cnt, int st, int en) {
-    if(cnt == 1) {
+void hanoi(int n, int st, int en) {
+    if(n==1) {
         answer.push_back({st, en});
         return;
     }
-    hanoi(cnt -1, st, 6 - st - en);
+    hanoi(n-1, st, 6 - st -en);
     answer.push_back({st, en});
-    hanoi(cnt -1, 6 - st - en, en);
+    hanoi(n-1, 6-st-en, en);
 }
 vector<vector<int>> solution(int n) {
     hanoi(n, 1, 3);
