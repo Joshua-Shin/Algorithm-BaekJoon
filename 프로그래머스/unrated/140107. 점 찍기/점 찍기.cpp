a5727cc1f56c06f7a79ec3784f3807b9268fd1f4
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-02-28 16:16:03 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.cpp
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 typedef long long ll;
 long long solution(int k, int d) {
-    long long answer = 0;
+    ll answer = 0;
     for(int i = 0; i * k <= d; i++) {
         ll x = i * k;
-        ll y = sqrt((ll)d*d - (ll)x*x);
-        answer += floor(y / k) + 1;
+        ll y = sqrt((ll)d * d - x * x);
+        answer += (y / k) + 1;
     }
     return answer;
 }
