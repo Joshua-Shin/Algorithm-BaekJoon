e3ddde0935754a722fb874fcfeba115522b645c9
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-04 13:10:50 +0900

    [level 2] Title: N-Queen, Time: 223.54 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int answer;
 int n;
-bool check[15][15];
-int dx[] = {-1, -1, 1, -1};
-int dy[] = {1, -1, 0, 0};
+bool check[13][13];
+int dx[] = {-1, -1, -1};
+int dy[] = {1, 0, -1};
 bool isOk(int x, int y) {
-    for(int k = 0; k < 4; k++) {
-        int nx = x, ny = y;
+    for(int k = 0; k < 3; k++) {
+        int nx = x;
+        int ny = y;
         while(1) {
             nx += dx[k];
             ny += dy[k];
-            if(nx < 0 || n <= nx || ny < 0 || n <= ny) break;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) break;
             if(check[nx][ny]) return false;
         }
     }
     return true;
 }
-void dfs(int x) { // 행
-    if(x == n) {
-        answer++;
-        return;
-    }
+int go(int x) { // 행
+    if(x == n) return 1;
+    int ret = 0;
     for(int i = 0; i < n; i++) { // 열
-        if(check[x][i] || !isOk(x, i)) continue;
+        if(check[x][i]) continue;
+        if(!isOk(x, i)) continue;
         check[x][i] = true;
-        dfs(x + 1);
+        ret += go(x + 1);
         check[x][i] = false;
     }
+    return ret;
 }
 int solution(int n) {
     ::n = n;
-    dfs(0);
-    return answer;
-}
-int main() {
-    cout << solution(4);
+    return go(0);    
 }
