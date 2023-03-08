cf01f8386abd350c2e42db773f60b31d6ebf7c99
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-08 19:40:57 +0900

    [level 3] Title: 풍선 터트리기, Time: 11.12 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/README.md
+++ 프로그래머스/lv3/68646. 풍선 터트리기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 풍선 터트리기 - 68646 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68646) 
 
 ### 성능 요약
 
-메모리: 46.3 MB, 시간: 11.03 ms
+메모리: 46.2 MB, 시간: 11.12 ms
 
 ### 구분
 
--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -1,22 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MX = 1'000'000;
 int solution(vector<int> a) {
     int answer = 0;
-    int lMin[MX], rMin[MX];
-    lMin[0] = a[0];
-    for(int i = 1; i < a.size(); i++)
-        lMin[i] = min(lMin[i-1], a[i]);
-    rMin[a.size()-1] = a[a.size()-1];
-    for(int i = a.size()-2; i >= 0; i--)
-        rMin[i] = min(rMin[i+1], a[i]);
-    for(int i = 0; i < a.size(); i++) {
-        if(i == 0 || i == a.size()-1) {
+    int n = a.size();
+    int lMin[n], rMin[n];
+    lMin[0] = a[0]; rMin[n-1] = a[n-1];
+    for(int i = 1; i < n; i++) lMin[i] = min(lMin[i-1], a[i]);
+    for(int i = n-2; i >= 0; i--) rMin[i] = min(rMin[i+1], a[i]);
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == n - 1) {
             answer++;
             continue;
         }
-        if(a[i] < lMin[i-1] || a[i] < rMin[i+1])
-            answer++;
+        if(a[i] < lMin[i-1] || a[i] < rMin[i+1]) answer++;
     }
     return answer;
 }
