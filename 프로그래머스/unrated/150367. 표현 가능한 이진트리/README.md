0125b9b798b045b6a87412a6be9e40debebada6f
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-12 20:15:54 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 17.98 ms, Memory: 4.69 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [unrated] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
 
 ### 성능 요약
 
-메모리: 4.86 MB, 시간: 17.76 ms
+메모리: 4.69 MB, 시간: 17.98 ms
 
 ### 구분
 
--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-bool dfs(int st, int en, bool prevNodeIsDummy, string &str) {
-    if(st > en) return true;
-    int mid = (st + en) / 2;
-    if(str[mid]=='1' && prevNodeIsDummy) return false;
-    return dfs(st, mid-1, str[mid]=='0', str) && dfs(mid+1, en, str[mid]=='0', str);
-}
-string convertBinary(ll n) {
+string str;
+string convert(ll num) {
     string ret;
-    while(n) {
-        ret += to_string(n % 2);
-        n /= 2;
+    while(num) {
+        ret += to_string(num % 2);
+        num /= 2;
     }
     reverse(ret.begin(), ret.end());
     return ret;
 }
+bool dfs(int st, int en, bool isDummy) {
+    if(st > en) return true;
+    int mid = (st + en) / 2;
+    if(str[mid] == '1' && isDummy) return false;
+    return dfs(st, mid-1, str[mid]=='0') && dfs(mid+1, en, str[mid]=='0');
+}
 vector<int> solution(vector<long long> numbers) {
     vector<int> answer;
     for(auto num: numbers) {
-        string str = convertBinary(num);
+        str = convert(num);
+        int diff;
         for(int i = 0; ; i++) {
-            int diff = (pow(2, i) -1) - str.size();
-            if(diff < 0) continue;
-            while(diff--) str = '0' + str;
+            if(pow(2, i) -1 < str.size()) continue;
+            diff = pow(2, i) -1 - str.size();
             break;
         }
-        dfs(0, str.size() - 1, false, str) ? answer.push_back(1) : answer.push_back(0);
+        while(diff--) str = '0' + str;
+        dfs(0, str.size()-1, false) ? answer.push_back(1): answer.push_back(0);
     }
     return answer;
 }
