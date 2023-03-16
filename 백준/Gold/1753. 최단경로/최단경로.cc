97c0bbcc388a4b0ebb6d70b6cf78770d4f8a10a7
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-16 17:47:23 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9108 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n; // 정점개수
-vector<pair<int, int>> adj[20001]; // 정점, 가중치
+int n;
+vector<pair<int, int>> adj[20001]; // 정점, 비용
 vector<int> dijkstra(int start) {
-    // 비용, 정점 // 비용이 가장 적은애가 top으로 올라옴.
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
-    // 정점번호가 1부터 시작할때, dist[x]: start에서 x정점까지 이동하는 최단 비용.
-    vector<int> dist(n + 1, 2e9); 
+    vector<int> dist(n+1, 2e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // 비용, 정점
     dist[start] = 0;
     pq.push({0, start});
-    while (!pq.empty()) {
-        int cost = pq.top().first;
+    while(!pq.empty()) {
         int x = pq.top().second;
-        pq.pop(); // 다익스트라에서는 여기에서 방문이 이루어진다고 봐야하는게 맞는듯.
-        // if (cost > dist[x]) continue; // 최적화인듯..?
+        int cost = pq.top().first;
+        pq.pop();
         for(auto e: adj[x]) {
             int nx = e.first;
             int nCost = cost + e.second;
