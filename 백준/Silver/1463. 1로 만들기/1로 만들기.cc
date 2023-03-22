cec57ff075a33482e0d8b4d41f64e9dff2658b30
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-22 11:17:54 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -5,8 +5,7 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n;
     int dp[1000001];
-    dp[2] = dp[3] = 1;
-    for(int i = 4; i<=n; i++) {
+    for(int i = 2; i<=n; i++) {
         int result = dp[i -1] + 1;
         if(i % 3 == 0) result = min(result, dp[i / 3] + 1);
         if(i % 2 == 0) result = min(result, dp[i / 2] + 1);
