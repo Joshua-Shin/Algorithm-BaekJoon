8206b3e6503fcc6ce94fd838d55557ecc44daba3
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-03-26 16:11:36 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 0.09 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.cpp
+++ 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.cpp
@@ -1,23 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int minCost = 1e9;
+vector<vector<int>> board;
 int dx[] = {0, -1, 0, 1};
 int dy[] = {1, 0, -1, 0};
-int go(string &board, int start, int end) {
-    int dist[16];
+int dist[4][4];
+int bfs(pair<int, int> a, pair<int, int> b, vector<vector<int>> &board) {
+    int x = a.first; int y = a.second;
+    int x2 = b.first; int y2 = b.second;
+    queue<pair<int, int>> q;
     memset(dist, -1, sizeof(dist));
-    queue<int> q;
-    dist[start] = 0;
-    q.push(start);
+    dist[x][y] = 0;
+    q.push({x, y});
     while(!q.empty()) {
-        start = q.front();
-        if(start == end) return dist[start];
+        tie(x, y) = q.front();
         q.pop();
-        int x = start / 4;
-        int y = start % 4;
+        if(x == x2 && y == y2) return dist[x][y];
         for(int k = 0; k < 4; k++) {
-            int nx = x, ny = y;
-            int nCur;
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if(nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
+            if(dist[nx][ny] != -1) continue;
+            dist[nx][ny] = dist[x][y] + 1;
+            q.push({nx, ny});
+        }
+        for(int k = 0; k < 4; k++) {
+            int nx = x; 
+            int ny = y;
             while(1) {
                 nx += dx[k];
                 ny += dy[k];
@@ -26,58 +34,50 @@ int go(string &board, int start, int end) {
                     ny -= dy[k];
                     break;
                 }
-                nCur = nx * 4 + ny;
-                if(board[nCur] != '0') break;
+                if(board[nx][ny] != 0) break;
             }
-            nCur = nx * 4 + ny;
-            if(dist[nCur] != -1) continue;
-            dist[nCur] = dist[start] + 1;
-            q.push(nCur);
-        }
-        for(int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if(nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
-            int nCur = nx * 4 + ny;
-            if(dist[nCur] != -1) continue;
-            dist[nCur] = dist[start] + 1;
-            q.push(nCur);
+            if(dist[nx][ny] != -1) continue;
+            dist[nx][ny] = dist[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-    return dist[end];
+    return dist[x2][y2];
 }
 int solution(vector<vector<int>> board, int r, int c) {
-    string strBoard;
-    vector<int> v;
-    vector<int> card[7];
-    for(int i = 0; i < 4; i++) 
-        for(int j = 0; j< 4; j++) {
-            strBoard += to_string(board[i][j]);
-            if(board[i][j] != 0) card[board[i][j]].push_back(i*4 + j);
+    vector<pair<int, int>> card[7];
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(board[i][j]!=0) {
+                card[board[i][j]].push_back({i, j});
+            }
         }
-    for(int i = 1; i <= 6; i++) {
+    }
+    vector<int> v;
+    for(int i = 1; i<=6; i++) {
         if(!card[i].empty()) v.push_back(i);
     }
-    int n = v.size(); // 카드 종류 개수
+    // d[x][0] : (card[v[x]][1] 제거 -> card[v[x]][0] 제거) 비용의 최소값
+    // d[x][1] : (card[v[x]][0] 제거 -> card[v[x]][1] 제거) 비용의 최소값
+    int n = v.size();
+    int ans = 1e9;
     do {
-        string strBoard2 = strBoard;
-        int cur = r * 4 + c;
         int d[6][2];
-        d[0][0] = go(strBoard2, cur, card[v[0]][0]) + go(strBoard2, card[v[0]][0], card[v[0]][1]);
-        d[0][1] = go(strBoard2, cur, card[v[0]][1]) + go(strBoard2, card[v[0]][1], card[v[0]][0]);
-        strBoard2[card[v[0]][0]] = '0';
-        strBoard2[card[v[0]][1]] = '0';
+        vector<vector<int>> myBoard = board;
+        d[0][0] = bfs({r, c}, card[v[0]][1], myBoard) + bfs(card[v[0]][1], card[v[0]][0], myBoard);
+        d[0][1] = bfs({r, c}, card[v[0]][0], myBoard) + bfs(card[v[0]][0], card[v[0]][1], myBoard);
+        myBoard[card[v[0]][1].first][card[v[0]][1].second] = 0;
+        myBoard[card[v[0]][0].first][card[v[0]][0].second] = 0;
         for(int i = 1; i < n; i++) {
-            d[i][0] = min(d[i-1][0] + go(strBoard2, card[v[i-1]][1], card[v[i]][0]),
-                            d[i-1][1] + go(strBoard2, card[v[i-1]][0], card[v[i]][0])) 
-                            + go(strBoard2, card[v[i]][0], card[v[i]][1]);
-            d[i][1] = min(d[i-1][0] + go(strBoard2, card[v[i-1]][1], card[v[i]][1]),
-                            d[i-1][1] + go(strBoard2, card[v[i-1]][0], card[v[i]][1])) 
-                            + go(strBoard2, card[v[i]][1], card[v[i]][0]);
-            strBoard2[card[v[i]][0]] = '0';
-            strBoard2[card[v[i]][1]] = '0';
+            d[i][0] = min(d[i-1][0] + bfs(card[v[i-1]][0], card[v[i]][1], myBoard),
+                            d[i-1][1] + bfs(card[v[i-1]][1], card[v[i]][1], myBoard))
+                            + bfs(card[v[i]][1], card[v[i]][0], myBoard);
+            d[i][1] = min(d[i-1][0] + bfs(card[v[i-1]][0], card[v[i]][0], myBoard),
+                            d[i-1][1] + bfs(card[v[i-1]][1], card[v[i]][0], myBoard))
+                            + bfs(card[v[i]][0], card[v[i]][1], myBoard);
+            myBoard[card[v[i]][1].first][card[v[i]][1].second] = 0;
+            myBoard[card[v[i]][0].first][card[v[i]][0].second] = 0;                 
         }
-        minCost = min({minCost, d[n-1][0], d[n-1][1]});
+        ans = min({ans, d[n-1][0], d[n-1][1]});
     } while (next_permutation(v.begin(), v.end()));
-    return minCost + n * 2;
+    return ans + v.size()*2;
 }
