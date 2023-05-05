6fb9241c4e9f7d2072d86c0bbd95e06d5f413327
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-05-05 16:05:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int y = x;
+        while(x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return y % sum == 0;
+    }
+}
