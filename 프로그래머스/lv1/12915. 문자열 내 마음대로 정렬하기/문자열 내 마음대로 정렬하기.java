7596d28a81ec26a430868d3ab96d28bd8d9cec64
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-05-06 12:56:19 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.48 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,13 +1,15 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, new Comparator<String>() {
-            public int compare(String a, String b) {
-                if(a.charAt(n) != b.charAt(n))
-                    return a.charAt(n) - b.charAt(n);
-                return a.compareTo(b);
-            }
+        Arrays.sort(strings, new Comparator<String>(){
+           public int compare(String a, String b) {
+               if(a.charAt(n) != b.charAt(n)) {
+                   return a.charAt(n) - b.charAt(n);
+               }
+               return a.compareTo(b);
+           }
         });
-        return strings; 
+        return strings;
     }
 }
