f40fee6980b2f09aca4f12bf46e93817d9b23ea0
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-05-06 12:59:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int result1 = gcd(n, m);
+        int result1 = gcm(n, m);
         int result2 = n * m / result1;
-        int[] answer = {result1, result2};
+        int [] answer = {result1, result2};        
         return answer;
     }
-    
-    public int gcd(int a, int b) {
+    public int gcm(int a, int b) {
         if(b == 0) return a;
-        return gcd(b, a % b);
+        return gcm(b, a % b);
     }
-    
 }
