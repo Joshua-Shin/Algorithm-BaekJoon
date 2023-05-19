ec216ddf5e7fccfc0f6a5909442b3c6a0878f685
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-05-19 18:04:27 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n;
+    if(n % 2) cout << "SK" << '\n';
+    else cout << "CY" << '\n';
+}
