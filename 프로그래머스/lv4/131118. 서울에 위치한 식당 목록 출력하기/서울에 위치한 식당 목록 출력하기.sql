d9ea57e804b344c01d927fc63ee403051e365529
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-06-16 22:03:56 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,7 @@
+select i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, 
+    round(avg(r.REVIEW_SCORE), 2) as SCORE
+from REST_INFO i join REST_REVIEW r
+    on i.REST_ID = r.REST_ID
+where i.ADDRESS like '서울%'
+group by i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS
+order by score desc, i.FAVORITES desc;
