5abe5a3a4efe81394d7c0d18d88dbb1ab3425e93
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-06-26 17:38:11 +0900

    [Gold II] Title: 도미노 (Small), Time: 20 ms, Memory: 2380 KB -BaekjoonHub

--- 백준/Gold/14586. 도미노 （Small）/도미노 （Small）.cc
+++ 백준/Gold/14586. 도미노 （Small）/도미노 （Small）.cc
@@ -1,87 +1,97 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 typedef long long ll;
+typedef vector<pair<int,int> > vii;
+typedef vector<pair<ll,ll> > vll;
+
+#define mp make_pair
+#define pb(x) push_back(x)
+#define all(x) (x).begin(),(x).end()
 
 const ll LLINF = 1e18;
 
-int Left[500000];
-int Right[500000];
-vector<pair<ll, ll>> v;
+int Left[301];
+int Right[301];
+vll v;
 
-int findLeft(int here) {
+int findLeft(int here){
 
     int &ret = Left[here];
-    if (ret != -1) return ret;
+    if(ret!=-1) return  ret;
 
     ll a = v[here].first;
     ll b = v[here].second;
 
-    int t = lower_bound(v.begin(), v.end(), make_pair(a - b, -1ll)) - v.begin();
+    int idx = lower_bound(all(v),mp(a-b, 0ll))-v.begin();
 
-    ret = t;
-    for (; t < here; t++)
-        ret = min(ret, findLeft(t));
+    
+    ret = idx;
+    for(;idx<here;idx++) ret = min(ret, findLeft(idx));
     return ret;
+    
+    
+//    for(int i = idx; i < cur; i++) {
+//        ret = min(ret, findLeft(i));
+//    }
+//    return ret;
 }
 
-int findRight(int here) {
+int findRight(int here){
 
     int &ret = Right[here];
-    if (ret != -1) return ret;
+    if(ret!=-1) return  ret;
 
     ll a = v[here].first;
     ll b = v[here].second;
 
-    int t = upper_bound(v.begin(), v.end(), make_pair(a + b, LLINF)) - v.begin();
+    int t = upper_bound(all(v),mp(a+b,LLINF))-v.begin();
 
-    ret = t - 1;
-    for (; here < t; here++)
-        ret = max(ret, findRight(here));
+    ret = t-1;
+    for(;here<t;here++) ret = max(ret, findRight(here));
     return ret;
 }
 
-int cache[301][301]; // 도미노 블록 개수가 지금 최대 300개라 이리 한듯
-int func(int a, int b) {
+int dp[301][301];
+int func(int a, int b){
 
-    int &ret = cache[a][b];
-    if (ret != -1) return ret;
+    int &ret= dp[a][b];
 
-    if (a > b) return ret = 0;
-    if (Right[a] >= b || Left[b] <= a) return ret = 1;
+    if(ret!=-1) return ret;
 
-    ret = 1e9;
-    for (int i = a; i <= b; i++)
-        ret = min(ret, func(a, Left[i] - 1) + func(Left[i], Right[i]) + func(Right[i] + 1, b));
+    if(a>b) return ret=0;
+    if(Right[a]>=b||Left[b]<=a) return ret=1;
+    
+    ret=1e9;
+    for(int i=a;i<=b;i++)
+        ret = min(ret, func(a,Left[i]-1)+func(Left[i],Right[i])+func(Right[i]+1,b));
     return ret;
 }
 
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
+int main(){
 
-    int n; // 도미노 개수
-    cin >> n;
+    int n;
+    scanf("%d",&n);
+
+    memset(dp,-1,sizeof(dp));
+
     v.resize(n);
-
-    memset(cache, -1, sizeof(cache));
-
-    ll a, b;
-    for (int i = 0; i < n; i++) {
-        cin >> a >> b; // x좌표, 막대 높이
-        v[i] = {a, b};
+    ll a,b;
+    for(int i=0;i<n;i++){
+        scanf("%lld %lld",&a,&b);
+        v[i] = mp(a,b);
     }
-    sort(v.begin(), v.end());
+    sort(all(v));
 
-    memset(Left, -1, sizeof(Left));
-    memset(Right, -1, sizeof(Right));
+    memset(Left,-1,sizeof(Left));
+    memset(Right,-1,sizeof(Right));
 
-    for (int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++){
         findLeft(i);
         findRight(i);
     }
 
-    // 모든 도미노를 넘어뜨리기 위해서 넘어뜨려야하는 최소 개수
-    cout << func(0, n - 1) << '\n';
-
+    printf("%d\n", func(0,n-1));
     return 0;
 }
