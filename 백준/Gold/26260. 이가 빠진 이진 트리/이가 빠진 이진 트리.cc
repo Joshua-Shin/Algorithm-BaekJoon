d7b0374ba02de01d5fc28d3b2eca47ad1dc87153
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-07-23 18:35:34 +0900

    [Gold V] Title: 이가 빠진 이진 트리, Time: 40 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Gold/26260. 이가 빠진 이진 트리/이가 빠진 이진 트리.cc
+++ 백준/Gold/26260. 이가 빠진 이진 트리/이가 빠진 이진 트리.cc
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int arr[140000];
-vector<int> ans;
+vector<int> v;
+
 void go(int st, int en) {
-    if(st >= en) return;
-    // if(st == en -1) {
-    //     ans.push_back(arr[st]);
-    //     return;
-    // }
-    int mid = (st + en) /2;
+    if(st + 1 == en) {
+        cout << v[st] << ' ';
+        return;
+    }
+    int mid = (st + en) / 2;
     go(st, mid);
-    go(mid+1, en);
-    ans.push_back(arr[mid]);
+    go(mid + 1, en);
+    cout << v[mid] << ' ';
 }
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n;
-    int hideIdx;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        if(arr[i] == -1) hideIdx = i;
+    for (int i = 0; i < n + 1; i++) {
+        int temp;
+        cin >> temp;
+        if(temp == -1) continue;
+        v.push_back(temp);
     }
-    cin >> arr[hideIdx];
-    sort(arr, arr+n);
+    sort(v.begin(), v.end());
     go(0, n);
-    for(auto x: ans) cout << x << ' ';
-}
+} 
