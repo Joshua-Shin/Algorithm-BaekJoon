a9d165bfa37a7070463b0b1ee7e80a45e0f7ddb4
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-09-19 14:40:07 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9108 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-vector<pair<int, int>> adj[20001]; // 정점, 비용
-vector<int> dijkstra(int start) {
-    vector<int> dist(n+1, 2e9);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // 비용, 정점
+int n, m, start;
+vector<pair<int, int>> adj[20001];
+vector<int> solve() {
+    vector<int> dist(n + 1, INT_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     dist[start] = 0;
     pq.push({0, start});
     while(!pq.empty()) {
         int x = pq.top().second;
         int cost = pq.top().first;
         pq.pop();
-        for(auto e: adj[x]) {
+        if(cost != dist[x]) continue;
+        for(auto &e: adj[x]) {
             int nx = e.first;
             int nCost = cost + e.second;
             if(nCost >= dist[nx]) continue;
@@ -23,16 +24,15 @@ vector<int> dijkstra(int start) {
 }
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    int m, st;
-    cin >> n >> m >> st;
-    for (int j = 0; j < m; j++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj[a].push_back({b, c});
+    cin >> n >> m >> start;
+    for(int i = 0; i < m; i++) {
+        int a, b, cost;
+        cin >> a >> b >> cost;
+        adj[a].push_back({b, cost});
     }
-    auto ans = dijkstra(st);
-    for(int i = 1; i<ans.size(); i++) {
-        if(ans[i] == 2e9) cout << "INF" << '\n';
-        else cout << ans[i] << '\n';
+    auto v = solve();
+    for(int i = 1; i <= n; i++) {
+        if(v[i] == INT_MAX) cout << "INF" << '\n';
+        else cout << v[i] << '\n';
     }
 }
