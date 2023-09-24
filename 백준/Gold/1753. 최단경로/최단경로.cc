8ce4d45bc330c0740e0dc394abd8fc10ef1bb1de
신중혁 (Shin JoongHyuk) <sjh910805@gmail.com>
2023-09-24 18:04:32 +0900

    [Gold IV] Title: 최단경로, Time: 312 ms, Memory: 8992 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,38 +1,53 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-int n, m, start;
-vector<pair<int, int>> adj[20001];
-vector<int> solve() {
-    vector<int> dist(n + 1, INT_MAX);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int n, m;
+vector <pair<int, int> > adj[20001]; // 정점, 가중치
+vector<int> dijkstra(int start)
+{
+    priority_queue <pair<int, int>> pq; // -비용 정점
+    vector <int> dist(n+1, 2e9); // 이걸 전역변수로 선언할 수 없는게, n 때문에 그럼. n의 값이 main 함수에서 입력을 받아서 수가 생기는건데, 입력을 받기전에 전역으로 초기화를 해버리면 n 값이 0일때 초기화가 되는거잖아.
     dist[start] = 0;
     pq.push({0, start});
-    while(!pq.empty()) {
+    while(!pq.empty())
+    {
         int x = pq.top().second;
-        int cost = pq.top().first;
-        pq.pop();
-        if(cost != dist[x]) continue;
-        for(auto &e: adj[x]) {
-            int nx = e.first;
-            int nCost = cost + e.second;
-            if(nCost >= dist[nx]) continue;
-            dist[nx] = nCost;
-            pq.push({dist[nx], nx});
+        int xCost = -pq.top().first;
+        pq.pop(); // 다익스트라에서는 여기에서 방문이 이루어진다고 봐야하는게 맞는듯.
+        if(xCost > dist[x]) continue; 
+        for(int i =0; i<adj[x].size(); i++)
+        {
+            int nx = adj[x][i].first;
+            int nxCost = xCost + adj[x][i].second;
+            if(nxCost < dist[nx])
+            {
+                dist[nx] = nxCost;
+                pq.push({-nxCost, nx});
+            }
         }
     }
     return dist;
 }
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-    cin >> n >> m >> start;
-    for(int i = 0; i < m; i++) {
-        int a, b, cost;
-        cin >> a >> b >> cost;
-        adj[a].push_back({b, cost});
+
+int main()
+{
+    cin >> n >> m;
+    int start;
+    cin >> start;
+    for(int i =0; i<m; i++)
+    {
+        int from, to, w;
+        cin >> from >> to >> w;
+        adj[from].push_back({to, w});
     }
-    auto v = solve();
-    for(int i = 1; i <= n; i++) {
-        if(v[i] == INT_MAX) cout << "INF" << '\n';
-        else cout << v[i] << '\n';
+    vector<int> dist = dijkstra(start);
+    for(int i =1; i<n+1; i++)
+    {
+        if(dist[i]!=2e9)
+            cout << dist[i] << '\n';
+        else
+            cout << "INF" << '\n';
     }
+    return 0;
 }
